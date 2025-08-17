d650790f810b669c220d32dac10c2155ff517ffb
MANISHBRO500 <160089365+MANISHBRO500@users.noreply.github.com>
2025-08-17 18:56:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,11 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+// Allow CORS only for your frontend
+app.use(cors({
+  origin: "https://manishbro500.github.io"
+}));
 
 // Routes
 const objectRoutes = require("./routes/objects");
