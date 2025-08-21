06cf393915dd7c5e18baf108e98146aa0df95901
MANISHBRO500 <160089365+MANISHBRO500@users.noreply.github.com>
2025-08-21 10:53:01 +0530

    Delete backend directory

--- backend/models/ObjectItem.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ObjectItemSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  quantity: { type: Number, required: true, default: 1 }
-});
-
-module.exports = mongoose.model("ObjectItem", ObjectItemSchema);
